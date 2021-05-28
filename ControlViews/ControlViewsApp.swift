696e2a12b86fba9f14b7b72104ce29ecfd4f1480
anilkaracay <anil.karacay06@hotmail.com>
2021-05-28 08:44:42 +0300

    Initial Commit

---
+++ ControlViews/ControlViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ControlViewsApp.swift
+//  ControlViews
+//
+//  Created by Macbook Air on 28.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ControlViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
